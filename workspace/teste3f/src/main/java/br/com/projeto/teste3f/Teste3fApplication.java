94eb0b96e564a8138607c86ecf6b36bdb7392d00
Gustavo Alves da Silva <114627774+Gustavo-alvin@users.noreply.github.com>
2024-02-26 08:03:53 -0300

    Add files via upload

---
+++ workspace/teste3f/src/main/java/br/com/projeto/teste3f/Teste3fApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.teste3f;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste3fApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste3fApplication.class, args);
+	}
+
+}
